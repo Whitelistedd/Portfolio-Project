8afc30a5e11a6d657957aa8052d366c78cfafa97
Volxenn <geforcexx.y.y@gmail.com>
2022-03-24 23:57:41 +0300

    Updated

--- myscripts.js
+++ myscripts.js
@@ -1,3 +1,23 @@
-middle = document.getElementById('middle')
+function alle (list) {
+    for (let i = 0; i < cars.length; i++) {
+        return i
+    }
+}
 
-middle.scrollIntoView()
+
+
+  console.log(i)
+
+x = document.getElementsByTagName("h2")
+
+function helo () {
+    document.getElementById("header").classList.toggle("header2");
+    document.getElementById("dark1").classList.toggle("dark2");
+    document.getElementById("middle").classList.toggle("middle2");
+    document.getElementById("service").classList.toggle("service2");
+    document.getElementById("Resume").classList.toggle("Resume2");
+    document.getElementById("page2").classList.toggle("page2-2");
+    document.getElementById("text1").classList.toggle("text1");
+    document.getElementById("text2").classList.toggle("text2");
+    document.getElementById("text3").classList.toggle("text3");
+}
