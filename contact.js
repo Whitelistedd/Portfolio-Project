2df8ed67211a52b95f048cb97982136c81a683c6
Volxenn <geforcexx.y.y@gmail.com>
2022-03-26 13:32:39 +0300

    Fixed contact Page
    Fixed header of contact page and darkmode

--- contact.js
+++ contact.js
@@ -1,11 +1,14 @@
 function helo () {
-    document.getElementById("header").classList.toggle("header2");
+    document.getElementById("header5").classList.toggle("header4");
+    document.getElementById("contact-section").classList.toggle("contact-section2");
     document.getElementById("dark1").classList.toggle("dark2");
     document.getElementById("middle").classList.toggle("middle2");
     document.getElementById("service").classList.toggle("service2");
     document.getElementById("Resume").classList.toggle("Resume2");
-    document.getElementById("page2").classList.toggle("page2-2");
     document.getElementById("text1").classList.toggle("text1");
     document.getElementById("text2").classList.toggle("text2");
     document.getElementById("text3").classList.toggle("text3");
+    document.getElementById("text-box1").classList.toggle("text-box2");
+    document.getElementById("text-box2").classList.toggle("text-box2");
+    document.getElementById("text-area").classList.toggle("textarea2");
 }
