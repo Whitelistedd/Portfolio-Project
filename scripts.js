a72510465df455df5aee3ce545be855d695ffda5
Volxenn <geforcexx.y.y@gmail.com>
2022-04-08 23:34:50 +0300

    Updated my portfolio
    Added a burger style menu for navigation on mobile, and made it more responsive for other devices, also changed the color styles and made the JS simpler, still working to add more stuff.

---
+++ scripts.js
@@ -0,0 +1,9 @@
+const darkmodeToggle = () => {
+    document.querySelector("body").classList.toggle("darkmode2")
+    document.getElementById("AboutMePage").classList.toggle("darkmode3")
+}
+
+const Menu = () => {
+    document.getElementById("Menu").classList.toggle("active");
+    document.getElementById("nav-menu").classList.toggle("active");
+}
