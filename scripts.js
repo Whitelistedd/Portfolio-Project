8275c512871d498dfd0acce120349beb8f1bdf71
Volxenn <geforcexx.y.y@gmail.com>
2022-04-09 17:59:52 +0300

    Added project section
    added project section and trying to add a better contact page

--- scripts.js
+++ scripts.js
@@ -1,9 +1,5 @@
 const darkmodeToggle = () => {
     document.querySelector("body").classList.toggle("darkmode2")
     document.getElementById("AboutMePage").classList.toggle("darkmode3")
-}
-
-const Menu = () => {
-    document.getElementById("Menu").classList.toggle("active");
-    document.getElementById("nav-menu").classList.toggle("active");
+    document.getElementById("contact-section2").classList.toggle("darkmode4")
 }
