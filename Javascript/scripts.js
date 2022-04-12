51df6018eb4fea947cdd963517284c52642561b4
Volxenn <geforcexx.y.y@gmail.com>
2022-04-13 02:18:51 +0300

    added a project
    added project 2 but still working on it and the project 3

--- Javascript/scripts.js
+++ Javascript/scripts.js
@@ -3,3 +3,10 @@ const darkmodeToggle = () => {
     document.getElementById("AboutMePage").classList.toggle("darkmode3")
     document.getElementById("contact-section2").classList.toggle("darkmode4")
 }
+
+const Menu = () => {
+    document.getElementById("Menu").classList.toggle("active");
+    document.getElementById("nav-menu").classList.toggle("active");
+}
+
+const options = {}
